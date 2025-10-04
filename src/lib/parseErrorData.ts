bf10fd1813da75b7579de69dd8ec92542f1538da
FloerSoloer <floer.solo.03@gmail.com>
2025-10-04 16:11:05 +0800

    ZodError: parse all ZodErrors the way /tags?/default did

--- src/lib/parseErrorData.ts
+++ src/lib/parseErrorData.ts
@@ -1,3 +1,4 @@
+import { ZodError } from 'zod';
 import { ErrorWithNotif, type TNotifParams } from './components/Notifs.store';
 
 type THandler = (e: any) => false | TNotifParams;
@@ -6,6 +7,15 @@ export function parseErrorNotif(prefix: string, e: any, handler?: THandler) {
 	if (e instanceof ErrorWithNotif) {
 		if (e.message) console.error(e.message);
 		return e.data;
+	}
+	if (e instanceof ZodError) {
+		const data: TNotifParams = { title: 'Invalid fields' };
+		const invalid_fields = new Set();
+		for (const issue of e.issues) for (const field of issue.path) invalid_fields.add(field);
+		if (invalid_fields.size > 0) data.msg = Array.from(invalid_fields).toSorted().join(', ');
+		// No invalid_fields is unexpected...
+		else console.error(prefix, e);
+		return data;
 	}
 
 	if (typeof handler !== 'undefined')
--- src/routes/tags/+page.server.ts
+++ src/routes/tags/+page.server.ts
@@ -2,8 +2,6 @@ import { pool, sql, z__tag } from '$lib/server/db/pool';
 import { UniqueIntegrityConstraintViolationError } from 'slonik';
 import type { Actions, PageServerLoad } from './$types';
 import { parseErrorNotif } from '$lib/parseErrorData';
-import { ZodError } from 'zod';
-import type { TNotifParams } from '$lib/components/Notifs.store';
 
 export const load: PageServerLoad = async function () {
 	const tags = pool.any(sql.typeAlias('tag')`SELECT * FROM tag`);
@@ -40,13 +38,6 @@ export const actions = {
 			} as const;
 		} catch (e) {
 			const data = parseErrorNotif('/tags/+page.server?/default', e, function (e) {
-				if (e instanceof ZodError) {
-					const data: TNotifParams = { title: 'Invalid fields' };
-					const invalid_fields = new Set();
-					for (const issue of e.issues) for (const field of issue.path) invalid_fields.add(field);
-					if (invalid_fields.size > 0) data.msg = Array.from(invalid_fields).toSorted().join(', ');
-					return data;
-				}
 				if (e instanceof UniqueIntegrityConstraintViolationError)
 					return { title: `${tag_name} already exists` };
 				return false;
