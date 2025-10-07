31acd4da11f1e04e42e245371c2ec9a9d376bb0a
FloerSoloer <floer.solo.03@gmail.com>
2025-10-07 20:40:03 +0800

    routes: add /tracks w/ New Track Cluster btn

---
+++ src/routes/tracks/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import short from 'short-uuid';
+	import IconMdiPlus from '~icons/mdi/plus';
+
+	const translator = short();
+	const next_qid = translator.new();
+</script>
+
+<a class="btn btn-primary" href={`/track/${next_qid}`} title="New Track Cluster">
+	<IconMdiPlus />
+</a>
